0bf265a998bf6e2a0a7108ea0506c5a7368434e7
Praveen Raja <116873039+PraveenRaja-2402@users.noreply.github.com>
2023-12-17 22:47:51 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
   plugins: [],
+  // Debug configuration
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts}"],
+    mode: 'all', // Include all classes for debugging
+    safelist: ['...'], // Optionally, safelist some classes
+  },
 };
